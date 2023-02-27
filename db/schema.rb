1dd96bcbc983ec13b857657737dcf0a28e9c2e5e
Andre Nelas <andrefnelas@gmail.com>
2023-02-27 18:09:26 +0000

    migrated bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_173436) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_180109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "num_of_pets"
+    t.string "pet_type"
+    t.datetime "start_date", precision: nil
+    t.datetime "end_date", precision: nil
+    t.integer "total_price"
+    t.bigint "user_id", null: false
+    t.bigint "listing_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_bookings_on_listing_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "listings", force: :cascade do |t|
     t.string "place_type"
@@ -40,5 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_173436) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "listings"
+  add_foreign_key "bookings", "users"
   add_foreign_key "listings", "users"
 end
